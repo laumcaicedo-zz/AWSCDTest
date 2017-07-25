ef54fce5d8e4134c20c1816a84b2f70e8fe3a417
Caicedo Camacho <lauracai@f45c899697e9.ant.amazon.com>
2017-07-25 10:34:25 -0500

    third

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-
 
 app = Flask(__name__)
 app = Flask(__name__)
-api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
         text = "Hello World!"
         return text
 
-api.add_resource(HelloWorld, '/hello/world')
 
 if __name__ == '__main__':
     # Runn Flask
